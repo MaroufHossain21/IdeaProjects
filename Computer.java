e85b1449fda0ed5ce7c374db24ab3b0c6b003562
Marouf Hossain <maroufhossain@Maroufs-MacBook-Pro.local>
2020-02-23 12:22:46 -0500

    First Commit

---
+++ Computer.java
@@ -0,0 +1,9 @@
+public class Computer {
+
+  public static void main(String[] args) {
+
+    System.out.println("I need to by a MacBook");
+
+   }
+
+}
---
+++ PhoneForFriends.java
@@ -0,0 +1,37 @@
+public class PhoneForFriends{
+
+  public static void main(String[] args) {
+
+    char phoneModel = 'M';
+
+    System.out.println("Model of the phone " + phoneModel);
+
+    // Yes = True, No = False
+    boolean loserFriendGetPhone = false;
+    // Anything with "" is String
+    System.out.println("Does my loser friends get any phone? " + loserFriendGetPhone);
+
+    boolean winnerFriendGetPhone = true;
+
+    System.out.println("Does my good friends get a phone? " + winnerFriendGetPhone);
+
+    //I want to buy 5 iPhones
+   //We can't keep any information/data without Container.
+   int numberOfPhone = 5;
+   int numberOfPhoneForMyFriend = 4;
+   int goodFriendPhone = 1;
+   int loserFriendPhone = 0;
+   //   9        =    5           +        4
+   int totalPhone = numberOfPhone + numberOfPhoneForMyFriend;
+
+   if (goodFriendPhone < 2);
+
+   System.out.println("How many phones does a good friend get? " + goodFriendPhone);
+   System.out.println("How many phones does a loser friend get? " + loserFriendPhone);
+
+    System.out.println("Total phone to buy? " + totalPhone);
+
+
+   }
+
+}
